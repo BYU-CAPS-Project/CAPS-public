1ef12f6d9911176209896c43eeeac1dcff660a6a
SethPeacock <msethpeacock@gmail.com>
2023-01-13 16:56:52 -0700

    fix data dict

--- scripts/make_data_dictionary_md.py
+++ scripts/make_data_dictionary_md.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-data_dict = pd.read_csv("DataDictionary.csv")
-text = data_dict.iloc[:, 0:6].to_markdown()
+data_dict = pd.read_csv("../data/DataDictionary.csv")
+text = data_dict.iloc[:, 0:7].to_markdown()
 
-with open("data_dictionary.md") as f:
+with open("../data_dictionary.md", 'w') as f:
     print(text, file=f)
