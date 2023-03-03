947740a01173dc1f4e8c94cfce6beaa0b3b46bc6
SethPeacock <msethpeacock@gmail.com>
2023-03-03 14:45:36 -0700

    Data dictionary moved

--- scripts/make_data_dictionary_md.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-data_dict = pd.read_csv("../data/DataDictionary.csv")
-text = data_dict.iloc[:, 0:7].to_markdown()
-
-with open("../data_dictionary.md", 'w') as f:
-    print(text, file=f)
